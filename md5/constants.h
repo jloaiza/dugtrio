8006830b7b72a9892fd94185ae32f85cc75abd17
arayaq <al.arayaq@gmail.com>
2013-05-07 18:28:44 -0600

    Constants for MD5Transform routine.

---
+++ md5/constants.h
@@ -0,0 +1,23 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Constants for MD5Transform routine.
+
+const int S11 = 7;
+const int S12 = 12;
+const int S13 = 17;
+const int S14 = 22;
+const int S21 = 5;
+const int S22 = 9;
+const int S23 = 14;
+const int S24 = 20;
+const int S31 = 4;
+const int S32 = 11;
+const int S33 = 16;
+const int S34 = 23;
+const int S41 = 6;
+const int S42 = 10;
+const int S43 = 15;
+const int S44 = 21;
+
+#endif // CONSTANTS_H
