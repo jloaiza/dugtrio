b5cfe06ae421b1949a5989f3becaf46a4ab519ae
Ángel Araya <al.arayaq@gmail.com>
2013-05-22 01:51:48 -0600

    Docs stubs

--- dugtrio/structures/ntree.h
+++ dugtrio/structures/ntree.h
@@ -1,10 +1,50 @@
+
 #ifndef NTREE_H
 #define NTREE_H
 
-class nTree
-{
+#include <string>
+
+class nTreeNode;
+class iFile;
+
+
+class nTree {
+
+private:
+
+    /**
+     * @brief _head
+     */
+    nTreeNode   _head;
+
 public:
-    nTree();
+
+    /**
+     * @brief create
+     * @param pName
+     * @param pFile
+     * @param pPath
+     */
+    void create(std::string pName, iFile* pFile, std::string pPath);
+
+    /**
+     * @brief erase
+     * @param pPath
+     */
+    void erase(std::string pPath);
+
+    /**
+     * @brief getNode
+     * @param pPath
+     * @return
+     */
+    nTreeNode* getNode(std::string pPath);
+
+    /**
+     * @brief getHead
+     * @return
+     */
+    nTreeNode* getHead();
 };
 
 #endif // NTREE_H
