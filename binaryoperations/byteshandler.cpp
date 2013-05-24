d1bea9f9d66faea4596a079e618a12dbb1ccc717
jloaiza <joseph.lo.cr@gmail.com>
2013-05-23 23:10:38 -0600

    Actualizacion

--- binaryoperations/byteshandler.cpp
+++ binaryoperations/byteshandler.cpp
@@ -12,13 +12,17 @@ std::string BytesHandler::snum2bin(long pNum, int pSize){
 
 std::string BytesHandler::string2bin(std::string pString, int pSize){
 
+	std::cout<<"entry:"<<(int)pString[0]<<" "<<(int)pString[1]<<std::endl;
+	std::cout<<"entry:"<<pString.length()<<std::endl;
+
 	std::string buffer;
+	pString += BaseConverter::multChar(0, pSize*8 - pString.length()*8);
+
+	std::cout<<"entry:"<<pString.length()<<std::endl;
 
 	for (int i = 0; i < pString.length(); i++){
 		buffer += BaseConverter::unum2bin(pString[i], 8);
 	}
-
-	buffer += BaseConverter::multChar(0, pSize*8 - buffer.length());
 
 	return buffer;
 }
@@ -26,26 +30,23 @@ std::string BytesHandler::string2bin(std::string pString, int pSize){
 std::string BytesHandler::bin2str(std::string pBinary){
 	
 	std::string buffer;
-
-	bool onConvertion = true;
 	char ichar;
 
-	for (int i = 0; onConvertion && i + 8 <= pBinary.length(); i += 8){
+	for (int i = 0; i + 8 <= pBinary.length(); i += 8){
 		ichar = BaseConverter::to_ulong(pBinary.substr(i, 8));
-		if (ichar == 0){
-			onConvertion = false;
-		} else {
-			buffer += ichar;
-		}
+		buffer += ichar;
 	}
 
 	return buffer;
 }
 
 int main(){
-	std::string binary = BytesHandler::string2bin("a", 1);
-	std::string prueba = "1";
-	std::cout<<binary.length()<<std::endl;
-	std::cout<<"l:"<<prueba.length()<<" "<<BytesHandler::bin2str(binary)<<std::endl;
+	std::string d = "123456789";
+	std::cout<<d.find('9')<<std::endl;
+	std::cout<<BytesHandler::string2bin("Hello", 10)<<std::endl;
+	std::cout<<BytesHandler::bin2str(BytesHandler::string2bin("Hello", 10))<<std::endl;
+
+	std::cout<<BytesHandler::bin2str("0000000000000001")<<std::endl;
+	std::cout<<BytesHandler::string2bin(BytesHandler::bin2str("0000000000000001"), 2)<<std::endl;
 	return 0;
 }
