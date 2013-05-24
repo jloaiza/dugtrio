6831533c6a35573829d85696cdc1f83b63bda5a1
jloaiza <joseph.lo.cr@gmail.com>
2013-05-24 01:15:06 -0600

    Lipiado de basura

--- dugtrio/tokenizer.cpp
+++ dugtrio/tokenizer.cpp
@@ -1,6 +1,9 @@
 #include "tokenizer.h"
 
 int Tokenizer::charIndex(std::string pString, char pChar, int pNumber){
+	if (pNumber == -1){
+		return -1;
+	}
 	int index = -1;
 	for (;pNumber > 0;pNumber--){
 
@@ -28,8 +31,14 @@ std::string Tokenizer::getCommandSpace(std::string pString, int pNumber){
 	return pString.substr(start, final-start);
 }
 
-std::string Tokenizer::getRegName(std::string pRegister){
+std::string Tokenizer::getRegister(std::string pRegisters, int pRegNum){
+	int prevSemicolon = charIndex(pRegisters, ';', pRegNum-1);
+	int nextSemicolon = charIndex(pRegisters, ';', pRegNum);
 
+}
+
+std::string Tokenizer::getRegName(std::string pRegister){
+	
 }
 
 std::string Tokenizer::getRegType(std::string pRegister){
